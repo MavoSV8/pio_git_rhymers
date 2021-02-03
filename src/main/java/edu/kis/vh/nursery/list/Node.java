4c08b535bc42d947d9ca67ec433e462c3455f3bb
MavoSV8 <Lukaszpasi@gmail.com>
2021-02-04 00:07:37 +0100

    1.4.3.4 - Owner - Deleted setters

--- src/main/java/edu/kis/vh/nursery/list/Node.java
+++ src/main/java/edu/kis/vh/nursery/list/Node.java
@@ -21,16 +21,9 @@ public class Node {
     public Node getPrev() {
         return prev;
     }
-
-    public void setPrev(Node prev) {
-        this.prev = prev;
-    }
-
+    
     public Node getNext() {
         return next;
     }
 
-    public void setNext(Node next) {
-        this.next = next;
-    }
 }
